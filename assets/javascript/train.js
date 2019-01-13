02b19f9bc866b2bba2a7a2b868c60c32093a907a
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2019-01-13 16:37:45 -0500

    close

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -15,11 +15,14 @@ var name = "";
 var des = "";
 var first = "";
 var freq = "";
+var firstArray= [];
+var freqArray=[];
+var counter = 0;
 
 $("#go").on("click", function(e){
 
     e.preventDefault();
-
+    
     name = $("#name").val().trim();
     des = $("#des").val().trim();
     first = $("#first").val().trim();
@@ -60,13 +63,22 @@ database.ref().on("child_added", function (childInfo){
     console.log(childInfo.val().first);
     console.log(childInfo.val().freq);
     console.log(childInfo.key);
+    
+
 
     var key = childInfo.key
     var username = childInfo.val().name;
     var userdes = childInfo.val().des;
     var userfreq = childInfo.val().freq;
-    // Time Stuff
+    var userfirst = childInfo.val().first;
     
+    firstArray.push(userfirst)
+    freqArray.push(userfreq)
+    console.log(firstArray)
+    console.log(freqArray)
+    
+
+    // Time Stuff
     var firstTime = moment(childInfo.val().first, "HH:mm").format("hh:mma");
     var timeConvert = moment(firstTime,"hh:mma").subtract(7, "days");
     var freqMin = childInfo.val().freq;
@@ -77,23 +89,19 @@ database.ref().on("child_added", function (childInfo){
     var lastArrival = moment().subtract(remaining, "minutes").format("h:mma")
     
 
-    
    
-       
+    
     $("#showMe").append("<tr><td>" 
 
         + username + "</td><td>"
         + userdes + "</td><td>"
-        + userfreq + "</td><td>"
-        + lastArrival + "</td><td>"
-        + nextArrival + "</td><td>"
+        + userfreq + "</td><td id='lastarr" + counter+ "'>"
+        + lastArrival + "</td><td id='nextarr" + counter +"'>"
+        + nextArrival + "</td><td id='left"+counter+"'>"
         + left + "</td><td><button class='btn btn-info btn-sm mb-2 mt-1' data-key='"+key+"' id='delete'>X</button>"
         + "</tr>"
     );
-    
-
-    
-    
+    counter++
 },function(errorObject) {
     console.log(errorObject.code);
 });
@@ -108,3 +116,22 @@ $(document).on("click", "#delete", function() {
 
 currentTime();
 
+setInterval(function (){
+
+    for (i=0 ; i<firstArray.length; i++) {
+
+    var firstTime = moment(firstArray[i], "HH:mm").format("hh:mma");
+    var timeConvert = moment(firstTime,"hh:mma").subtract(7, "days");
+    var freqMin = freqArray[i];
+    var difference = moment().diff(timeConvert, "minutes");
+    var remaining = difference % freqMin;
+    var left = freqMin - remaining;
+    var nextArrival = moment().add(left, "minutes").format("h:mma")
+    var lastArrival = moment().subtract(remaining, "minutes").format("h:mma")
+
+    $("#lastarr"+[i]).text(lastArrival)
+    $("#nextarr"+[i]).text(nextArrival)
+    $("#left"+[i]).text(left)
+    }
+
+}, 60000);
